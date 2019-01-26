0d5e3db5ea155d27be5dcd9bcda93ccf41b70436
Deven96 <diretnandomnan@gmail.com>
2019-01-26 03:17:28 +0100

    timeit

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+import time
 from handlers import Verifier, Uploader
 
+start = time.time()
 uploader = Uploader("Any Name")
-print("Uploader Class works")
+print("Uploader Class works, initialized in {time.time()-start}")
+start = time.time()
 ver = Verifier()
-print("Verifier Class works")
+print("Verifier Class works, initialized in {time.time()-start}")
